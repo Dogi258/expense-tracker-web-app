206f3a2b9b6448753bb96868e5387c663991db4a
Dogi258 <mtz.daniel21@gmail.com>
2022-03-18 22:34:46 -0500

    Added card component to extract duplicate css

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,4 +1,5 @@
 import "./ExpenseDate.css";
+import Card from "./Card";
 
 export default function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
@@ -6,10 +7,10 @@ export default function ExpenseDate(props) {
   const year = props.date.getFullYear();
 
   return (
-    <div className="expense-date">
+    <Card className="expense-date">
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__day">{day}</div>
       <div className="expense-date__year">{year}</div>
-    </div>
+    </Card>
   );
 }
